ac6f39b14387d3f65e08f4fc2b19fcc29366c0d7
caiwp <caiwenpi@gmail.com>
2021-07-07 17:12:56 +0800

    feat: update limit 8k to 6MB

--- server.go
+++ server.go
@@ -5,7 +5,7 @@ import (
 	"net"
 )
 
-const maxBufferSize = 2 << 12 // 8k
+const maxBufferSize = 2 << 15 // 6MB
 
 func ListenUDP(address string, handler Handler) error {
 	pc, err := net.ListenPacket("udp", address)
